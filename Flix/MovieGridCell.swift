91fe16e022e6c656a1d17c077e04a802d44f1928
AnaSantos7 <asantos@csumb.edu>
2019-09-29 21:23:50 -0700

    completed part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ana Santos on 9/23/19.
+//  Copyright © 2019 Ana Santos. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flix/MovieViewController.swift
+++ Flix/MovieViewController.swift
@@ -59,4 +59,15 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         cell.posterView.af_setImage(withURL: posterUrl!)
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
